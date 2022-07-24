1b29304c17714fc15781ed5531a96652288f5148
Anton Stanovoi <a.stanovoi170993@gmail.com>
2022-07-24 19:30:13 +0300

    Updates

---
+++ precache-manifest.14544b38cb080e0799d9873fb7b83968.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "27b6db115100a6a98a93",
+    "url": "/static/css/main.6be98638.chunk.css"
+  },
+  {
+    "revision": "27b6db115100a6a98a93",
+    "url": "/static/js/main.27b6db11.chunk.js"
+  },
+  {
+    "revision": "dd688a8cdf5beaac8aeb",
+    "url": "/static/js/1.dd688a8c.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "f98d75245d3da4823954058996a2d334",
+    "url": "/index.html"
+  }
+];
